2ee5487ea6bb58c0443c7cea22cc7a0f5b3f56a5
Tomás González Amer <tomas.gonzalez.amer@hotmail.com>
2023-06-24 18:49:44 -0300

    segunda entrega

--- Js/app.js
+++ Js/app.js
@@ -1,33 +1,107 @@
-// Generar un número aleatorio entre 1 y 100
-let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+// Declaración de variables
+let puntajeJugador = 0;
+let puntajeComputadora = 0;
 
-// Función para iniciar el juego
-function jugarJuego() {
-  let intento = prompt("Adivina el número (entre 1 y 100):");
-  let numero = parseInt(intento);
+// Array de opciones posibles
+let opciones = ["piedra", "papel", "tijera"];
 
-  // Comprobar si el jugador ingresó un número válido
-  if (isNaN(numero)) {
-    alert("Por favor, ingresa un número válido.");
-    jugarJuego(); // Volver a solicitar un intento
-    return;
-  }
+// Función para obtener una elección aleatoria de la computadora
+function obtenerEleccionComputadora() {
+  let indiceAleatorio = Math.floor(Math.random() * opciones.length);
+  return opciones[indiceAleatorio];
+}
 
-  // Comprobar si el jugador adivinó el número
-  if (numero === numeroAleatorio) {
-    alert("¡Felicidades! Adivinaste el número.");
+// Función para determinar el ganador de una ronda
+function determinarGanador(eleccionJugador, eleccionComputadora) {
+  if (
+    (eleccionJugador === "piedra" && eleccionComputadora === "tijera") ||
+    (eleccionJugador === "papel" && eleccionComputadora === "piedra") ||
+    (eleccionJugador === "tijera" && eleccionComputadora === "papel")
+  ) {
+    return "jugador";
+  } else if (
+    (eleccionJugador === "tijera" && eleccionComputadora === "piedra") ||
+    (eleccionJugador === "piedra" && eleccionComputadora === "papel") ||
+    (eleccionJugador === "papel" && eleccionComputadora === "tijera")
+  ) {
+    return "computadora";
   } else {
-    // Proporcionar pistas al jugador
-    let mensaje = "Incorrecto. ";
-    if (numero < numeroAleatorio) {
-      mensaje += "Intenta con un número más alto.";
-    } else {
-      mensaje += "Intenta con un número más bajo.";
-    }
-    alert(mensaje);
-    jugarJuego(); // Volver a solicitar un intento
+    return "empate";
   }
 }
 
-// Iniciar el juego
-jugarJuego();
+// Objeto Jugador
+let jugador = {
+  puntaje: 0,
+  eleccion: "",
+  actualizarPuntaje: function () {
+    this.puntaje++;
+  },
+  reiniciarPuntaje: function () {
+    this.puntaje = 0;
+  }
+};
+
+// Objeto Computadora
+let computadora = {
+  puntaje: 0,
+  eleccion: "",
+  actualizarPuntaje: function () {
+    this.puntaje++;
+  },
+  reiniciarPuntaje: function () {
+    this.puntaje = 0;
+  }
+};
+
+// Función principal del juego
+function jugarPiedraPapelTijera() {
+  alert("¡Bienvenido al juego de Piedra, Papel o Tijera!");
+
+  while (true) {
+    // Obtener la elección del jugador
+    let eleccionJugador = prompt("Elegí: piedra, papel o tijera. Para salir, escribí 'salir'.");
+
+    if (eleccionJugador === null || eleccionJugador.toLowerCase() === "salir") {
+      alert("Gracias por jugar. Nos vemos!");
+      break;
+    }
+
+    // Validar la elección del jugador
+    if (!opciones.includes(eleccionJugador)) {
+      alert("Elección inválida. Intenta nuevamente.");
+      continue;
+    }
+
+    // Generar elección de la computadora
+    let eleccionComputadora = obtenerEleccionComputadora();
+
+    // Asignar elecciones al jugador y la computadora
+    jugador.eleccion = eleccionJugador;
+    computadora.eleccion = eleccionComputadora;
+
+    // Determinar el ganador de la ronda
+    let ganador = determinarGanador(jugador.eleccion, computadora.eleccion);
+
+    // Actualizar puntajes y mostrar resultados
+    if (ganador === "jugador") {
+      jugador.actualizarPuntaje();
+      alert("¡Ganaste esta ronda!");
+    } else if (ganador === "computadora") {
+      computadora.actualizarPuntaje();
+      alert("La computadora ganó esta ronda.");
+    } else {
+      alert("Empate en esta ronda.");
+    }
+
+    // Mostrar elecciones y puntajes
+    alert("Elección del jugador: " + jugador.eleccion);
+    alert("Elección de la computadora: " + computadora.eleccion);
+    alert("Puntaje actual:");
+    alert("Jugador: " + jugador.puntaje);
+    alert("Computadora: " + computadora.puntaje);
+  }
+}
+
+// Llamar a la función para iniciar el juego
+jugarPiedraPapelTijera();
