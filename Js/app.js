ca1b9f97233cd7febd822d1d9db0b3f4ef10df5c
Tomás González Amer <tomas.gonzalez.amer@hotmail.com>
2023-09-04 21:03:14 -0300

    primer entrega

--- Js/app.js
+++ Js/app.js
@@ -1,50 +1,51 @@
 function calcular() {
-    const operacion = prompt("Ingrese una operación (+, -, *, /) o 'salir'");
-    
-    if (operacion === 'salir') {
-        alert("Calculadora finalizada.");
-        return;
-    }
+    let continuar = true;
 
-    if (operacion !== '+' && operacion !== '-' && operacion !== '*' && operacion !== '/') {
-        alert("Operación no válida. Por favor, ingrese +, -, *, / o 'salir'.");
-        calcular();
-        return;
-    }
+    while (continuar) {
+        const operacion = prompt("Ingrese una operación (+, -, *, /) o 'stop' para detenerse:");
 
-    const numero1 = parseFloat(prompt("Ingrese el primer número:"));
-    const numero2 = parseFloat(prompt("Ingrese el segundo número:"));
-
-    if (isNaN(numero1) || isNaN(numero2)) {
-        alert("Por favor, ingrese números válidos.");
-        calcular();
-        return;
-    }
-
-    let resultado;
-    
-    switch (operacion) {
-        case '+':
-            resultado = numero1 + numero2;
+        if (operacion === 'stop') {
+            alert("Calculadora finalizada.");
+            continuar = false;
             break;
-        case '-':
-            resultado = numero1 - numero2;
-            break;
-        case '*':
-            resultado = numero1 * numero2;
-            break;
-        case '/':
-            if (numero2 === 0) {
-                alert("No se puede dividir por cero.");
-                calcular();
-                return;
-            }
-            resultado = numero1 / numero2;
-            break;
-    }
+        }
 
-    alert(`Resultado: ${resultado}`);
-    calcular();
+        if (operacion !== '+' && operacion !== '-' && operacion !== '*' && operacion !== '/') {
+            alert("Operación no válida. Por favor, ingrese +, -, *, / o 'stop'.");
+            continue;
+        }
+
+        const numero1 = parseFloat(prompt("Ingrese el primer número:"));
+        const numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+
+        if (isNaN(numero1) || isNaN(numero2)) {
+            alert("Por favor, ingrese números válidos.");
+            continue;
+        }
+
+        let resultado;
+
+        switch (operacion) {
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                if (numero2 === 0) {
+                    alert("No se puede dividir por cero.");
+                    continue;
+                }
+                resultado = numero1 / numero2;
+                break;
+        }
+
+        alert(`Resultado: ${resultado}`);
+    }
 }
 
-calcular();
+calcular(); // Iniciar la calculadora
